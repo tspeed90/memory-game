fe41ee342b7e869d1ce8d521f2299988e0519892
tspeed90 <tspeed90@gmail.com>
2018-08-21 21:40:55 +0100

    reduces grid from 16 to 12 tiles and improves mobile responsiveness

--- src/handlers.js
+++ src/handlers.js
@@ -7,7 +7,7 @@ const staticHandler = (response, filepath) => {
   const extension = filepath.split('.')[1];
   const extensionType = {
     html: 'text/html',
-    css: 'text/css', 
+    css: 'text/css',
     js: 'application/javascript',
     jpg: 'image/jpg',
     png: 'image/png',
@@ -16,47 +16,52 @@ const staticHandler = (response, filepath) => {
 
   fs.readFile(path.join(__dirname, '..', filepath), 'binary', (error, file) => {
     if (error) {
-      response.writeHead(500, {'Content-Type': 'text/html'});
-      response.end("<h1>Sorry, there was a problem on our end.</h1>");
+      response.writeHead(500, { 'Content-Type': 'text/html' });
+      response.end('<h1>Sorry, there was a problem on our end.</h1>');
     } else {
-      response.writeHead(200, {'Content-Type': extensionType[extension]});
+      response.writeHead(200, { 'Content-Type': extensionType[extension] });
       response.write(file, 'binary');
       response.end();
     }
   });
-}
+};
 
-const apiHandler = (response) => {
-  request(`https://pixabay.com/api/?key=${process.env.API_KEY}&q=nature&editors_choice=true&page=1`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-      response.writeHead(500,{'Content-Type': 'text/html'});
-      response.end('<h1>Sorry, we had a problem.</h1>');
-    } else {
-      response.writeHead(200, {'Content-Type': 'application/json'});
-      response.end(JSON.stringify(getImages(body)));
+const apiHandler = response => {
+  request(
+    `https://pixabay.com/api/?key=${
+      process.env.API_KEY
+    }&q=nature&editors_choice=true&page=1`,
+    (err, _, body) => {
+      if (err) {
+        console.log(err);
+        response.writeHead(500, { 'Content-Type': 'text/html' });
+        response.end('<h1>Sorry, we had a problem.</h1>');
+      } else {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(getImages(body)));
+      }
     }
-  });
-}
+  );
+};
 
-const getImages = (body) => {
+const getImages = body => {
   const data = JSON.parse(body);
-  while (data.hits.length > 8) {
+  while (data.hits.length > 6) {
     const randomNum = Math.floor(Math.random() * data.hits.length);
     data.hits.splice(randomNum, 1);
   }
   let images = data.hits.map(image => image.largeImageURL);
   return images;
-}
+};
 
-const notFoundHandler = (response) => {
-  response.writeHead(404, {'Content-Type' : 'text/html'});
-  response.end('<h1>Sorry, we couldn\'t find what you\'re looking for.</h1>');
-}
+const notFoundHandler = response => {
+  response.writeHead(404, { 'Content-Type': 'text/html' });
+  response.end("<h1>Sorry, we couldn't find what you're looking for.</h1>");
+};
 
 module.exports = {
   staticHandler,
   apiHandler,
-  notFoundHandler, 
+  notFoundHandler,
   getImages
-}
+};
